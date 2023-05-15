c35cdb917d483eaf9d61309727ec5c5fdb78b728
js <stolle.jan@gmail.com>
2023-05-15 16:17:58 +0200

    Update custom.js
    Fixed addEventListener to subscribe only once (on a selected function) rather than multiple times due to using an anonymous function.

--- shiny/www/custom.js
+++ shiny/www/custom.js
@@ -1,25 +1,28 @@
 function selectOrDeselectAllOfInputId(inputId, isChecked) {
-  var inputIdReal = inputId.replace("_allSelector","");
-  var allCheckboxes = document.getElementById(inputIdReal).getElementsByTagName("input");
+  var inputIdReal = inputId.replace("_allSelector","")
+  var allCheckboxes = document.getElementById(inputIdReal).getElementsByTagName("input")
   for(var i=0; i < allCheckboxes.length; ++i) {
-    allCheckboxes[i].checked = isChecked;
+    allCheckboxes[i].checked = isChecked
   }
-};
+}
 
 function checkOrUncheckAllSelectorIfNeeded(event) {
-  const inputId = event.target.name;
-  if (inputId == null || inputId.endsWith("_allSelector")) {
+  if(event.target.parentElement.parentElement.className != "checkbox") {
     return;
+  }
+  const inputId = event.target.name
+  if (inputId == null || inputId.endsWith("_allSelector")) {
+    return
   }
   var isClickedBoxChecked = event.target.checked;
-  var mainDiv = document.getElementById(inputId);
+  var mainDiv = document.getElementById(inputId)
   if( mainDiv == null) {
-    return;
+    return
   }
-  var allCheckboxes = mainDiv.getElementsByTagName("input");
-  var allSelectorExists = allCheckboxes[0].name.endsWith("_allSelector");
+  var allCheckboxes = mainDiv.getElementsByTagName("input")
+  var allSelectorExists = allCheckboxes[0].name.endsWith("_allSelector")
   if (allSelectorExists == false){
-    return;
+    return
   }
   for (var i=0; i < allCheckboxes.length; ++i) {
     if (i==0) {
@@ -27,17 +30,10 @@ function checkOrUncheckAllSelectorIfNeeded(event) {
     }
     if (allCheckboxes[i].checked !== isClickedBoxChecked) {
       allCheckboxes[0].checked = false;
-      return;
+      return
     }
   }
   allCheckboxes[0].checked = isClickedBoxChecked;
-};
+}
 
-
-document.addEventListener('click', function (event) {
-
-  if (event.target.type = "checkbox") {
-    checkOrUncheckAllSelectorIfNeeded(event);
-  }
-
-}, false);
+document.addEventListener('click', checkOrUncheckAllSelectorIfNeeded, false)
